16ee634ce557207164f4a7b3225475cbb8b71919
Christian <64144555+chr3st5an@users.noreply.github.com>
2021-03-17 13:48:10 +0100

    Add files via upload

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,11 @@
 function scrollToTop() {
 	return window.scrollTo(0, -10_000);
 }
+
+function openInstagram () {
+	return window.open("https://www.instagram.com/purefairytale/", "_blank");
+}
+
+function openSnapchat () {
+	return window.open("https://www.snapchat.com/add/fassungslose", "_blank");
+}
